4c9442fb052090cec4ae1c632e91d9d288ea404a
Brandur <brandur@mutelight.org>
2013-07-31 11:31:25 -0700

    Add parameter validation

--- lib/doc_stub/generator.rb
+++ lib/doc_stub/generator.rb
@@ -17,10 +17,19 @@ module DocStub
           method = action["method"]
           path   = action["path"]
           status = action["statuses"][0]
+          required_params =
+            action["attributes"] && action["attributes"]["required"]
+          optional_params =
+            action["attributes"] && action["attributes"]["optional"]
+          if required_params && !optional_params
+            optional_params = required_params
+          end
           # "{app}" to ":app"
           path.gsub!(/{([a-z_]*)}/, ':\1')
           #puts "method=#{method} path=#{path}"
           @app.send(method.downcase, path) do
+            require_params!(required_params) if required_params
+            validate_params!(optional_params) if optional_params
             [status, example]
           end
         end
--- lib/doc_stub/service_stub.rb
+++ lib/doc_stub/service_stub.rb
@@ -2,8 +2,27 @@ module DocStub
   class ServiceStub < Sinatra::Base
     use Rack::Instruments
 
-    def initialize(*args, &block)
-      super
+    before do
+      @body = MultiJson.decode(request.body.read) rescue {}
+    end
+
+    private
+
+    def require_params!(keys)
+      missing = []
+      keys.each do |k|
+        missing << k unless @body[k]
+      end
+      halt(400, MultiJson.encode(
+        message: "Require params: #{missing.join(', ')}."
+      )) if missing.size > 0
+    end
+
+    def validate_params!(keys)
+      extra = @body.keys - keys
+      halt(400, MultiJson.encode(
+        message: "Unknown params: #{extra.join(', ')}."
+      )) if extra.size > 0
     end
   end
 end
